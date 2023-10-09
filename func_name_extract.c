0155c976cadb2a07c924fbc7db40a941d67e19e9
danmooji <70057651+ANG-web@users.noreply.github.com>
2023-10-10 02:40:41 +0900

    Update func_name_extract.c

--- func_name_extract.c
+++ func_name_extract.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
-void func_name_parser(FILE* fs)
+void remove_a(char *str) { // 공백, 쌍따옴표, 콤마 제거
+    int i, j = 0;
+    int len = strlen(str);
+
+    for (i = 0; i < len; i++) {
+        if (str[i] != ' ' && str[i] != '"' && str[i] != ',') {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+}
+
+void func_name_extract(FILE* fs)
 {
     char str[100] = {0};
-    const char str_name[9] = "declname"; // 함수명을 가리키는 key
-    char func_name[10000] = {0};
-    size_t str_length = 0;
-    char* newline_ptr = 0;
     int i = 1;
 
-    // fs = fopen("ast.json", "r");
     if (fs == NULL) printf("파일 열기 실패\n");
 
     while (fgets(str, 100, fs) != NULL)
-    {   
-        char* name_ptr = strstr(str, str_name); // 함수 이름이 나올때마다 name_ptr에 위치 저장
+    {                                           // decl 노드 밑에 (_nodetype으로 불필요한거 걸러줌) name 속성 파싱
+        char* name_ptr = strstr(str, "decl"); 
         
-        if (name_ptr != NULL) {
-            char* newline_ptr = strchr(name_ptr, '\n'); // 개행이 나올 때까지의 길이 저장
-            str_length = newline_ptr - name_ptr; // 함수 이름이 있는 키 : 값 총 길이
-            strncpy(func_name, name_ptr, str_length-1);
+        if (name_ptr != NULL ) {
+            fgets(str, 100, fs);
+            name_ptr = strstr(str, "_nodetype");
+            if (name_ptr != NULL) {
 
-            name_ptr = strchr(func_name, ' '); // 키 : 값 중에 값, 함수 이름만 파싱
-            printf("%d번째 함수 이름 :%s\n", i, name_ptr);
-            i += 1;
+                for (int i = 0; i < 6; i++) fgets(str, 100, fs);
+                
+                name_ptr = strstr(str, "name");
+                name_ptr = strchr(name_ptr, ' ');
+                remove_a(name_ptr);
+                printf("%d번째 함수 이름 : %s\n", i, name_ptr);
+                i += 1;      
+            }
         }
-
     }
     fclose(fs);
 }
+
+/*int main() {
+
+    FILE* fs = fopen("ast.json", "r");
+    func_name_extract(fs);
+
+    return 0;
+}*/
